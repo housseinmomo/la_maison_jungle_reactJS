9992f834b7c9513ed341fe9d4b0368f39c7f23af
housseinmomo <smrobla34@gmail.com>
2023-01-20 20:10:40 +0100

    Ajout & stylisation de mon composant Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,14 @@
 import '../styles/Banner.css'
+import logo from '../assets/logo.png'
 
 
 function Banner () {
+
+    const title = "La maison de la jungle"
+
     return <div className="lmj-banner">
-        <h1>La maison de la jungle</h1>
+        <img src={logo} alt={title} className="lmj-logo" />
+        <h1 className='lmj-title'>{title}</h1>
     </div>
 }
 
