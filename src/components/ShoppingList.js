f7c7d4ee6425c4fb1014613c0aeb8155cd5f9b95
housseinmomo <smrobla34@gmail.com>
2023-01-22 16:07:15 +0100

    Stylisation du composant ShoppingList

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,5 @@
 import { plantList } from "../datas/plantList"
+import "../styles/ShoppingList.css"
 
 /**
  * 
@@ -9,29 +10,60 @@ Les keys :
 La documentation de React est claire sur ce sujet : les  key (clés) aident React à identifier quels éléments d’une liste ont changé,ont
 été ajoutés ou supprimés. Vous devez donner une clé à chaque élément dans un tableau, afin d’apporter aux éléments une identité stable.
 La  key   permet d'associer une donnée au composant correspondant dans le DOM virtuel qui permettra ensuite de générer les composants. 
+La prop  key   est indispensable dans les listes de composants 
+*/
+
+/**
+ * La fonction reduce():
+ * 
+ * - principe : La méthode reduce() applique une fonction qui est un « accumulateur » et qui traite chaque valeur d'une liste 
+ * (de la gauche vers la droite) afin de la réduire à une seule valeur.
+ * 
+ *  - accumulateur (acc) : La valeur précédemment retournée par le dernier appel du callback, ou valeurInitiale, si elle est fournie (voir ci-après) 
+ * (c'est la valeur « accumulée » au fur et à mesure des appels
+ * 
+ * - ValeurCourante : La valeur de l'élément courant actuellement manipulé dans le tableau.
+ *
+ * - ValeurInital : valeurInitialeFacultatif Une valeur utilisée comme premier argument lors du premier appel de la fonction callback.
+ *   Si aucune valeur initiale n'est fournie, le premier élément du tableau est utilisé (et la boucle de traitement ne le parcourera pas).
+
+The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+The includes() method returns true if an array contains a specified value. The includes() method returns false if the value is not found. The includes() method is case sensitive.
+ * 
  */
 
 function ShoppingList () {
 
     const categories = plantList.reduce(
 		(acc, plant) =>
+            // Si la categorie se trouve dans notre acc : on ne fait rien 
+            // Sinon on concatene la nouvelle categorie a notre acc 
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
-		[]
+		[] 
 	)
 
+    console.log(categories)
 
     return (
         <div>
+
+            {/* Recuperation des categorie */}
             <ul>
                 {categories.map((cat) => (
                     <li key={cat}>{cat}</li>
                 ))}
-                </ul>
-                <ul>
-                    {plantList.map((plant) => (
-                        <li key={plant.id}>{plant.name}</li>
-                    ))}
-                </ul>
+            </ul>
+
+            {/* Recuperation des plantes */}
+            <ul className="lmj-plant-list">
+                {plantList.map((plant) => (
+                    <li key={plant.id} className="lmj-plant-item">
+                        {plant.name} 
+                        {plant.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
+                    </li> 
+                ))}
+            </ul>
+
         </div>
     )
 }
